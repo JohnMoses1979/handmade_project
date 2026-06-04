606dbbc12191e9e6df224c2813742e9456c5c8ff
Ubuntu <ubuntu@ip-172-31-3-158.ap-south-2.compute.internal>
2026-06-04 04:52:10 +0000

    apk check

--- src/api/config.js
+++ src/api/config.js
@@ -23,6 +23,5 @@ const resolveBaseUrl = () => {
   return `http://${EC2_IP}/api`;
 };
 
-export const BASE_URL = resolveBaseUrl();
-
+export const BASE_URL = "http://16.112.231.38/api";
 export default BASE_URL;
